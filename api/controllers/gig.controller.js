9d1e42f5b4935dd63ea7af3d0539c13f0fbc8e61
Roshan Singh <Surender@trimir.in>
2023-04-20 16:22:23 +0530

    done gig part

---
+++ api/controllers/gig.controller.js
@@ -0,0 +1,55 @@
+import Gig from "../models/gig.model.js";
+import createError from "../utils/createError.js";
+
+export const createGig = async (req, res, next) => {
+  if (!req.isSeller)
+    return next(createError(400, " only seller can create a gig"));
+
+  const newGig = new Gig({ userId: req.userId, ...req.body });
+  try {
+    const savedGig = await newGig.save();
+    res.status(201).json({ message: "gig is created", ...savedGig });
+  } catch (error) {
+    next(error);
+  }
+};
+export const getGig = async (req, res, next) => {
+  try {
+    const gig = await Gig.findById(req.params.id);
+    if (!gig) next(createError(404, " no data found"));
+    res.status(200).json(gig);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getGigs = async (req, res, next) => {
+  const q = req.query;
+  const filters = {
+    ...(q.userId && { userId: q.userId }),
+    ...(q.cat && { cat: q.cat }),
+    ...((q.min || q.max) && {
+      price: { ...(q.min && { $gt: q.min }), ...(q.max && { $lt: q.max }) },
+    }),
+    ...(q.title && { title: { $regex: q.search, $options: "i" } }),
+  };
+  try {
+    const gigs = Gig.find(filters);
+    res.status(200).json(gigs);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteGig = async (req, res, next) => {
+  try {
+    const gig = await Gig.findById(req.params.id);
+    if (gig.userId !== req.userId)
+      return next(createError(404, " only creator of gig can delete it"));
+
+    await Gig.findByIdAndDelete(req.params.id);
+    res.status(200).send("Gig has been deleted!");
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/gig.route.js
+++ api/routes/gig.route.js
@@ -1,5 +1,16 @@
 import express from "express";
+import { verifyToken } from "../middlewares/jwt";
+import {
+  createGig,
+  deleteGig,
+  getGig,
+  getGigs,
+} from "../controllers/gig.controller";
 
 const router = express.Router();
 
+router.post("/", verifyToken, createGig);
+router.get("/:id", verifyToken, getGig);
+router.get("/", verifyToken, getGigs);
+router.delete("/:id", verifyToken, deleteGig);
 export default router;
